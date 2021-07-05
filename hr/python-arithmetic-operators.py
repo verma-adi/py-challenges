e83c43d72412a6d00ca9120086d9871b75c11133
verma-adi <adiverma31@outlook.com>
2021-07-05 20:48:36 +0530

    python-arithmetic-operators.py

---
+++ hr/python-arithmetic-operators.py
@@ -0,0 +1,7 @@
+# problem statement: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
